7d390835086aa94ca60f60f3b62fc7ac1c10d5a9
Twigga17 <neil.bomett17@mail.com>
2022-03-10 16:34:29 -0800

    ghbxa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,15 @@
 #include<stdio.h>
-
 /**
- * main - print in print function
+ * main - Entry point
  *
- * Description : using main function
- * this program prints "Size of types of various types"
- * Return:0
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c;
-int i;
-long li;
-long long lli;
-loat f;
-printf("Size of a char : %ld byte(s)\n", sizeof(c));
-printf(("Size of an int : % ld byte(s)\n", sizeof(i));
-printf("Size of a long int : % ld byte(s)\n", sizeof(li));
-printf("Size of a long long int : % ld byte(s)\n", sizeof(lli));
-printf("Size of a float : % ld byte(s)\n", sizeof(f));
+printf("Size of a char: % ld byte(s)", sizeof(char));
+printf("Size of an int: % ld byte(s)", sizeof(int));
+printf("Size of a long int: % ld bytes(s)", sizeof(long int));
+printf("Size of a long long int: % ld byte(s)", sizeof(long long int));
+printf("Size of a float: % ld byte(s)", sizeof(float));
 return (0);
 }
