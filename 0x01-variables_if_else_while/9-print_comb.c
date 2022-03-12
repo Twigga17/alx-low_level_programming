83593e436890773764fbeda99b2107a90ef82379
Twigga17 <neil.bomett17@mail.com>
2022-03-12 03:34:22 -0800

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 /**
  * main - main block
  * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line 
+ * starting from 0,followed by a new line
  * Return:0
  */
 int main(void)
@@ -10,6 +10,7 @@ int main(void)
 int c = 0;
 while (c < 10)
 {
+
 putchar(48 + c);
 if (c != 9)
 {
