c6bcf4b77ee6b1e6c4a75181c48ac09daa1ee903
Twigga17 <neil.bomett17@mail.com>
2022-03-15 09:12:16 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,14 @@
  */
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
-	char('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
