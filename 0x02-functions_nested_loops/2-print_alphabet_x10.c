440086edde6aef960fa98e451a4a3849715be4ca
Twigga17 <neil.bomett17@mail.com>
2022-03-15 11:18:38 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - check the code
- * Description: Print 10 times the alphabet 
+ * Description: Print 10 times the alphabet
  * in lowercase then followed by new line
  * Return: Always 0
  */
@@ -9,14 +9,10 @@ void print_alphabet_x10(void)
 {
 	char c = 'a';
 	while (c <= 'z')
+	
 	{
 		_putchar(c);
 		c++;
 	}
 	_putchar('\n');
-}
-int main()
-{
-	repeat ('c', 10);
-	return 0;
 }
