50665ccaa6cdf4cb4b455c35b799916872b33b00
Twigga17 <neil.bomett17@mail.com>
2022-03-15 09:30:25 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Prints the alphabet in lowercase 
+ * Followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
