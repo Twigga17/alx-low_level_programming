cbca9ca04974dd918a8441f46f7a404389c54015
Twigga17 <neil.bomett17@mail.com>
2022-03-15 19:45:14 -0700

    The end

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @a: int type number
+ * @b: int type type number
+ * Return: reult of addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
