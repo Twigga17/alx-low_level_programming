87bf0feaa66c356be0d5cac605d8757c15e2eab6
Twigga17 <neil.bomett17@mail.com>
2022-03-17 13:04:13 -0700

    L

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints fizzbuzz
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%i", n);
+	}
+	return (0);
+}
