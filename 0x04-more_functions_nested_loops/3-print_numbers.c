0531c44febd8202aef0be2c3912c23f507e920a8
Twigga17 <neil.bomett17@mail.com>
2022-03-17 05:45:55 -0700

    D

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ *
+ * followed by a new line
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar(10);
+}
