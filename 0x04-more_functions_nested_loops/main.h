1b7a85b89d1a3f03ef31730a39a8d0d16286d68b
Twigga17 <neil.bomett17@mail.com>
2022-03-17 16:06:22 -0700

    Z

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -15,7 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int isPrime(long int n);
-long int biggestFactor(long int a)
 
 #endif
