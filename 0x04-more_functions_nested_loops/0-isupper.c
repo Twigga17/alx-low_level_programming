bf51f33d5e8ee3fc5d58b44289a09f5533b98d91
Twigga17 <neil.bomett17@mail.com>
2022-03-17 03:57:25 -0700

    A

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: char type letter
+ *
+ * Return: 1 if c is uppercase, 0 if otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}	
