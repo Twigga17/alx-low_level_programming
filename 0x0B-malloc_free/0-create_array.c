ad33ea84b90b59da1871b8794e5a3a40fab0f727
Twigga17 <neil.bomett17@mail.com>
2022-04-04 01:55:06 -0700

    Malloc and Free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - Creates and initializes array with specific char
+ * @size: size of array
+ * @c: character to be initialized
+ * Return: pointer to an array or null
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Code for malloc free assignment
